a7e1317b9281ae179d5ad82dfefa9e8586db4f0d
skippharaoh <caniggiathompson@gmail.com>
2022-02-08 17:33:30 -0500

    Setup + Starting Instructions Complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import StarshipList from './components/StarshipsList ';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <StarshipList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StarshipsList .jsx
@@ -0,0 +1,9 @@
+function StarshipList(){
+    return(
+        <div>
+            <h2>Starship Component Rendering</h2>
+        </div>
+    )
+}
+
+export default StarshipList
